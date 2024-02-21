f61f5b73414e61c31946e06bdd240b88e8daa2da
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-21 16:06:39 +0100

    inserimento mail in pagina

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const { createApp } = Vue
 
 createApp({
-    
-})
+    data() {
+        return {
+            arrayEmails: [],
+        }
+    },
+    mounted() {
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risultato) => {
+                this.arrayEmails.push(risultato.data.response)
+            })
+        }
+    },
+}).mount("#app");
